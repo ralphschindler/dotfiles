be533472488bf733dedd5a2d07de64464f57864e
Ralph Schindler <ralph@ralphschindler.com>
2021-02-27 08:09:37 -0600

    updates

--- functions.zsh
+++ functions.zsh
@@ -218,3 +218,16 @@ function unquarantine() {
 		xattr -r -d "$attribute" "$@"
 	done
 }
+
+# Launch Tinkerwell in path
+function tinkerwell {
+  if [ $# -eq 0 ] || [ $1 = "." ]; then
+    TW_PATH=$(pwd)
+  else
+    TW_PATH=$1
+  fi
+
+  TW_PATH=$(echo -n $TW_PATH | base64)
+
+  open "tinkerwell://?cwd=$TW_PATH"
+}
